6e9b7403b6a96c9dd0524b8c06a94e0224d4217b
Anemachiso <101452814+Anemachiso@users.noreply.github.com>
2023-11-06 19:17:24 +0000

    datetime stamp API microservices

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,13 +19,31 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api/", function (req, res) {
+  let currentDate = new Date()
+  res.json({"unix": currentDate.getTime(), "utc": `${currentDate}`});
 });
 
+// console.log(date)
 
-
+app.get("/api/:date?", function (req, res) {
+  if (req.params.date === '' || req.params.date === undefined) {
+    const current = new Date();
+    return res.json({ unix: current.getTime(), utc: current.toUTCString(), });
+  }
+  
+  const date = new Date(req.params.date);
+  if (date.toString() === 'Invalid Date') {
+    const date2 = new Date(parseInt(req.params.date, 10));
+    if (date2.toString() === 'Invalid Date') {
+      return res.json({ error : "Invalid Date" });
+    }
+    return res.json({ unix: date2.getTime(), utc: date2.toUTCString(), });
+  }
+  return res.json({ unix: date.getTime(), utc: date.toUTCString(), });
+});
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
